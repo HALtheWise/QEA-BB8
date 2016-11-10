25d55dbdd0accdf9daf151e97bc97e4b77142afe
nathanyee <nathaniel.yee@students.olin.edu>
2016-11-10 12:27:18 -0500

    added sensor ino file with library include statements

--- Arduino v1/sensing_sensors/sensing_sensors.ino
+++ Arduino v1/sensing_sensors/sensing_sensors.ino
@@ -1,1 +1,10 @@
-arduinon codddeee
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+
+#include <I2Cdev.h>
+
+// script that allows us to read sensor data.
+
+

2be95678faa95545e29637b702f1c32da1a51b79
Eric Miller <eric@legoaces.org>
2016-11-17 10:44:02 -0500

    Work from in class  11/17

--- Arduino v1/framework/wireless.h
+++ Arduino v1/framework/wireless.h
@@ -7,9 +7,10 @@ SoftwareSerial btSerial(10, 11); // RX, TX
 
 void setupWireless(){
 
-  // set the data rate for the SoftwareSerial port
-  // NOTE The soft serial port can only run up to 19200 bps.
-  btSerial.begin(9600);
+	// set the data rate for the SoftwareSerial port
+	// NOTE The software serial port can only run up to 19200 bps.
+	// The JY-MCU bluetooth unit defaults to 9600 baud, which is used here.
+	btSerial.begin(9600);
 }
 
 void loopWireless() {
@@ -27,7 +28,7 @@ void loopWireless() {
 		byte terminator = btSerial.read();
 
 		if (start == '>' && identifier == '\n' && sensorType == 9){
-			Serial.println("good BT message");
+			Serial.println("good BT message");	
 		}else{
 			Serial.println("bad BT message");
 		}
@@ -35,6 +36,5 @@ void loopWireless() {
 		while (btSerial.available()){
 			btSerial.read();
 		}
-
 	}
 }

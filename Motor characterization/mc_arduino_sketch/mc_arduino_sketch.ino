330a196183a0126b3264ab5d02e211e05dcb6cbc
Eric Miller <eric@legoaces.org>
2016-12-07 15:31:42 -0500

    Prepared for motor characterization

--- Motor characterization/mc_arduino_sketch/mc_arduino_sketch.ino
+++ Motor characterization/mc_arduino_sketch/mc_arduino_sketch.ino
@@ -11,7 +11,46 @@ void setup() {
 void loop() {
 	printIfNewEncoder();
 	readSerial();
-	moveMotor(motorspeed * 255);
+	// moveMotor(motorspeed * 255);
+}
+
+const int timeout = 10000; // ms
+const int heightRead = 24; // encoder ticks
+const int heightStop = 48; // encoder ticks
+
+const int returnPower = -.2*256;
+
+/* Lifts weight, measuring speed and current and returning ticks/second */
+float characterizeMotion(int power) {
+	long starttime = millis();
+
+	// Start lifting weight
+	moveMotor(power);
+	while(encoder1.read() < heightRead && (millis() - starttime) < timeout) {}
+
+	// Take the reading
+	long readStartTime = millis();
+
+	while(encoder1.read() < heightStop && (millis() - readStartTime) < timeout) {}
+
+	long readStopTime = millis();
+
+	moveMotor(0);
+
+	float dt = (readStopTime - readStartTime)/1000.0;
+	float measuredspeed = (heightStop - heightRead) / dt;
+
+	Serial.print("Power: "); Serial.print(power); 
+	Serial.print("\tSpeed: "); Serial.println(measuredspeed);
+
+	delay(1000);
+
+	moveMotor(returnPower);
+
+	starttime = millis();
+	while(encoder1.read() > 0 && (millis() - readStartTime) < timeout) {}
+
+	moveMotor(0);
 }
 
 void readSerial(){

07fa039382b1bd086f8001c6daca0658ad94c2c4
Eric Miller <eric@legoaces.org>
2016-12-08 13:58:06 -0500

    Working first-pass of motor characterization sweep

--- Motor characterization/mc_arduino_sketch/mc_arduino_sketch.ino
+++ Motor characterization/mc_arduino_sketch/mc_arduino_sketch.ino
@@ -18,10 +18,38 @@ const int timeout = 5*1000; // ms
 const int heightRead = 24; // encoder ticks
 const int heightStop = 48; // encoder ticks
 
-const int holdPower = 0.10*256;
-const int returnPower = .06*256;
+int holdPower = 0;
+int returnPower = 0;
 
-/* Lifts weight, measuring speed and current and returning ticks/second */
+const float maxSpeed = 100;
+
+void manyCharacterize() {
+
+	int minPower = 0;
+	for(int power=0; power<256; power += 10){
+	    float result = characterizeMotion(power);
+	    if (result > 0) {
+	    	minPower = power;
+	    	moveMotor(0);
+	    	break;
+	    }
+	}
+	Serial.print("Discovered lower bound power: ");
+	Serial.println(minPower);
+
+	holdPower = minPower - 10;
+	returnPower = minPower - 30;
+
+
+	for(int power=minPower; power<256; power += 10){
+	    float result = characterizeMotion(power);
+	    if (result > maxSpeed) {
+	    	break;
+	    }
+	}
+}
+
+/* Lifts weight, measuring speed and current and returning speed in ticks/sec */
 float characterizeMotion(int power) {
 	encoder1.write(0);
 	long starttime = millis();
@@ -29,11 +57,21 @@ float characterizeMotion(int power) {
 	// Start lifting weight
 	moveMotor(power);
 	while(encoder1.read() < heightRead && (millis() - starttime) < timeout) {}
+	if ((millis() - starttime) >= timeout) {
+		moveMotor(0);
+		return 0;
+	}
+
 
 	// Take the reading
 	long readStartTime = millis();
 
+	starttime = millis();
 	while(encoder1.read() < heightStop && (millis() - readStartTime) < timeout) {}
+	if ((millis() - starttime) >= timeout) {
+		moveMotor(0);
+		return 0;
+	}
 
 	long readStopTime = millis();
 
@@ -50,27 +88,33 @@ float characterizeMotion(int power) {
 	moveMotor(returnPower);
 
 	starttime = millis();
-	while(encoder1.read() > 0 && (millis() - readStartTime) < timeout) {}
+	while(/*encoder1.read() > 0 &&*/ (millis() - readStartTime) < timeout) {}
 
 	moveMotor(0);
+	return measuredspeed;
 }
 
 void readSerial(){
 	Serial.setTimeout(20); //20ms timeout on serial reads
 	if(Serial.available()){
 	    motorspeed = Serial.parseFloat() / 100;
-	    Serial.print("Motor speed set to ");
-	    Serial.println(motorspeed);
 	    delay(3);
 
-	    bool isTest = (Serial.read()=='t');
+	    byte command = Serial.read();
 	    while(Serial.available()){
 	    	Serial.read();
 	    }
 
-	    if (isTest) {
+	    if (command=='t') {
+		    Serial.print("Test running at power ");
+		    Serial.println(motorspeed);
 	    	characterizeMotion(motorspeed * 255);
+	    } else if(command=='s'){
+		    Serial.println("Sweeping");
+	       	manyCharacterize();
 	    } else {
+		    Serial.print("Motor speed set to ");
+		    Serial.println(motorspeed);
 	    	moveMotor(motorspeed * 255);
 	    }
 	}
